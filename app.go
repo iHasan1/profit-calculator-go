9c00a38025750391aacf42b78c4779abf0b30baf
Imran Hasan <imran.h94@gmail.com>
2024-01-31 14:48:41 +0500

    finished building app

--- app.go
+++ app.go
@@ -1,8 +1,27 @@
 package main
 
 import "fmt"
-import "math"
+
+// import "math"
 
 func main() {
-	
+	var revenue, expenses, taxRate float64
+
+	fmt.Print("Enter your revenue: ")
+	fmt.Scan(&revenue)
+
+	fmt.Print("Enter your expenses: ")
+	fmt.Scan(&expenses)
+
+	fmt.Print("Enter the tax rate: ")
+	fmt.Scan(&taxRate)
+
+	earningsBeforeTax := revenue - expenses
+	profit := earningsBeforeTax - (revenue * taxRate/100)
+
+	fmt.Print("Your Earnings Before Tax (EBT) is:")
+	fmt.Print(earningsBeforeTax)
+	fmt.Println()
+	fmt.Print("Your Profit is: ")
+	fmt.Print(profit)
 }
