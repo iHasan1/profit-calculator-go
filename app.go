28b2e692b22c8ce9a8ec7366d31e5ce9a150b27f
Imran Hasan <imran.h94@gmail.com>
2024-02-01 12:12:40 +0500

    reformatted the code to add fuctions and optimize the code

--- app.go
+++ app.go
@@ -3,22 +3,28 @@ package main
 import "fmt"
 
 func main() {
-	var revenue, expenses, taxRate float64
+	revenue, expenses, taxRate := askForInput()
 
-	fmt.Print("Enter your revenue: ")
-	fmt.Scan(&revenue)
-
-	fmt.Print("Enter your expenses: ")
-	fmt.Scan(&expenses)
-
-	fmt.Print("Enter the tax rate: ")
-	fmt.Scan(&taxRate)
-
-	earningsBeforeTax := revenue - expenses
-	profit := earningsBeforeTax * (1 - taxRate/100)
-	ratio := earningsBeforeTax / profit
+	earningsBeforeTax, profit, ratio := calcValues(revenue, expenses, taxRate)
 
 	fmt.Println("Your Earnings Before Tax (EBT) is:", earningsBeforeTax)
 	fmt.Println("Your Profit is:", profit)
 	fmt.Printf("Ratio: %.2f", ratio)
+}
+
+func askForInput() (rev float64, exp float64, tR float64){
+	fmt.Print("Enter your revenue: ")
+	fmt.Scan(&rev)
+	fmt.Print("Enter your expenses: ")
+	fmt.Scan(&exp)
+	fmt.Print("Enter the tax rate: ")
+	fmt.Scan(&tR)
+	return
+}
+
+func calcValues (rev, exp, tR float64) (ebt, profit, ratio float64){
+	ebt = rev - exp
+	profit = ebt * (1 - tR/100)
+	ratio = ebt / profit
+	return
 }
