c3f2de2da81054d260d0bcb05bf3f19cbbc5e71e
Imran Hasan <imran.h94@gmail.com>
2024-02-06 11:29:37 +0500

    added saving to file functionality and managing errors

--- app.go
+++ app.go
@@ -1,23 +1,50 @@
 package main
 
 import "fmt"
+import "errors"
+import "os"
+
+func storeResults(ebt, profit, ratio float64){
+	results := fmt.Sprintf("EBT: %.1f\nProfit: %.1f\nRatio: %.3f", ebt, profit, ratio)
+	os.WriteFile("results.txt", []byte(results), 0644)
+}
 
 func main() {
-	revenue := getUserInput("Revenue: ")
-	expenses := getUserInput("Expenses: ")
-	taxRate := getUserInput("Tax Rate: ")
+	revenue, err := getUserInput("Revenue: ")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	expenses, err := getUserInput("Expenses: ")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	taxRate, err := getUserInput("Tax Rate: ")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
 	earningsBeforeTax, profit, ratio := calcValues(revenue, expenses, taxRate)
 
 	fmt.Printf("Your Earnings Before Tax (EBT) is: %.1f\n", earningsBeforeTax)
 	fmt.Printf("Your Profit is: %.1f\n", profit)
 	fmt.Printf("Ratio: %.3f\n", ratio)
+	storeResults(earningsBeforeTax, profit, ratio)
 }
 
-func getUserInput(infoText string) (userInput float64){
+func getUserInput(infoText string) (float64, error){
+	var userInput float64
 	fmt.Print(infoText)
 	fmt.Scan(&userInput)
-	return
+
+	if userInput <= 0 {
+		return 0, errors.New("value must be a positive number.")
+	}
+	return userInput, nil
 }
 
 func calcValues (rev, exp, tR float64) (ebt, profit, ratio float64){
